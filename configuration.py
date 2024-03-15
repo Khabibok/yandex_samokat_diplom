6bf14afb76e1c293d9bb2893670429db1a91bee1
Khabibok <163551920+Khabibok@users.noreply.github.com>
2024-03-15 17:30:21 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,8 @@
+# Адрес сервера
+URL_SERVICE = "https://39e6e469-f50c-45ed-8032-d8c47381e02b.serverhub.praktikum-services.ru"
+# Api Docs
+DOC_PATH = "/docs/"
+# Ручка создания набора
+ADD_ORDER_PATH = "/api/v1/orders"
+# Ручка создания пользователя
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
