8035075ac638cd9accd5bcad63817a874f763995
Khabibok <163551920+Khabibok@users.noreply.github.com>
2024-03-15 17:30:44 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,22 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+post_order_body = {
+        "firstName": "Иван",
+        "lastName": "Иванов",
+        "address": "Менделеевская",
+        "metroStation": 40,
+        "phone": "+7 999 999 88 88",
+        "rentTime": 4,
+        "deliveryDate": "2024-03-21",
+        "comment": "как можно быстрее",
+        "color": [
+            "BLACK"
+        ]
+    }
+
+
+get_track_params = {
+    't': ''
+}
