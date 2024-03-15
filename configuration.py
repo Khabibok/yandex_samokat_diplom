56ac32b8dc707fb12a33a9d78848d273a1607d0d
Khabibok <163551920+Khabibok@users.noreply.github.com>
2024-03-15 17:46:29 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,8 +1,8 @@
 # Адрес сервера
 URL_SERVICE = "https://39e6e469-f50c-45ed-8032-d8c47381e02b.serverhub.praktikum-services.ru"
+# Ручка создания пользователя
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
 # Api Docs
 DOC_PATH = "/docs/"
 # Ручка создания набора
 ADD_ORDER_PATH = "/api/v1/orders"
-# Ручка создания пользователя
-GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
